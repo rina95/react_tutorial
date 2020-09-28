eea38618747148653789bc4fa0aecc6897d8e6aa
oBuiThiPhan <bui.thi.phan@sun-asterisk.com>
2020-09-28 14:28:53 +0700

    config again heroku

--- server/server.js
+++ server/server.js
@@ -1,15 +1,31 @@
-const path = require('path');
+// const path = require('path');
+// const express = require('express');
+// const app = express();
+// const port = process.env.PORT || 3001;
+// const publicPath = path.join(__dirname, '..', 'public');
+
+// app.use(express.static(publicPath));
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(publicPath, 'index.html'));
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is up on port ${port}!`);
+// });
+
 const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
+const port = process.env.PORT || 8080;
 const app = express();
-const port = process.env.PORT || 3001;
-const publicPath = path.join(__dirname, '..', 'public');
 
-app.use(express.static(publicPath));
+app.use(favicon(__dirname + '/build/favicon.ico'));
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(publicPath, 'index.html'));
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Server is up on port ${port}!`);
-});
+app.listen(port);
